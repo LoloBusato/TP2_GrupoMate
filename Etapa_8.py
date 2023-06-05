48a276e018bb195be58e60413306685064f030fd
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-06-05 18:19:40 -0300

    Modify: JB, te dejo unas modificaciones del codigo para generar el diccionario, fijate si alguna te copa para implementar en el programa

--- Etapa_8.py
+++ Etapa_8.py
@@ -1,71 +1,43 @@
-import csv
-#-------------------------------------------------------------------------------------------------------------------
-import chardet
-#esta funcion la tuve que crear para resolver un problema que me saltaba en consola.
-#esta en desuso igual, lo que esta en uso es la constante ENCODING
-#la funcion encoding esta en cada open de archivos (hay 3) = ENCODING
-def detectar_encoding(archivo):
-    with open(archivo, 'rb') as f:
-        resultado = chardet.detect(f.read())
-    return resultado['encoding']
-
-ENCODING='utf-8'
-
-#-------------------------------------------------------------------------------------------------------------------
-
-#LEE LOS ARCHIVOS.TXT LINEA POR LINEA
-def leer_pal(palabras,indice_pal):
-    linea_pal=palabras.readline().rstrip()
-    indice_pal+=1
-    return linea_pal,indice_pal 
-
-def leer_def(definiciones,indice_def):
-    linea_def=definiciones.readline().rstrip()
-    indice_def+=1
-    return linea_def,indice_def 
-
-#CREACION DE NUESTRO DICC ORDENADO ALF.   TYPE: LIST
-def crear_diccionario(palabras,definiciones):
-    indice_pal=0
-    indice_def=0
-    dicc={}
-    palabra,indice_pal=leer_pal(palabras,indice_pal)
-    definicion,indice_def =leer_def(definiciones,indice_def)
-    while palabra:
-            #valido que la palabra sea alum.
-            if palabra.isalnum():
-                if indice_def==indice_pal:
-                    dicc[palabra]="'"+ str(definicion)+"'"
-                    definicion,indice_def =leer_def(definiciones,indice_def)
-                    palabra,indice_pal=leer_pal(palabras,indice_pal)
-                else:
-                    definicion,indice_def =leer_def(definiciones,indice_def)
-            else:
-                palabra,indice_pal=leer_pal(palabras,indice_pal)
-    
-    dicc=sorted(dicc.items(),key=lambda x:x[PALABRA])
-    return dicc
-
-#ABRIMOS LOS ARCHIVOS.TXT--------------------------------------------------------------------------------------------
-palabras_txt = open('palabras.txt', 'r', encoding=ENCODING)
-definiciones_txt = open('definiciones.txt', 'r', encoding=ENCODING)
 #----------------------------------------------------------------------------------------------------------------
 #GENERO VARIABLES Y CONSTANTES QUE VOY A USAR
-count_pal=0
-count_def=0
-PALABRA=0
-palabras_definiciones = crear_diccionario(palabras_txt,definiciones_txt)
+TAMANIO = 5
+
+#LEE LOS ARCHIVOS.TXT LINEA POR LINEA
+def leer_archivo(archivo):
+    linea = archivo.readline().rstrip()
+    return linea if linea else "" #Devuelve 1 vacio
+
+def grabar_archivo(archivo, diccionario):
+    PALABRA = 0
+    DEFINICION = 1
+    for pal_def in diccionario:
+        archivo.write(pal_def[PALABRA] + ',' + pal_def[DEFINICION] + '\n')
+
+#CREACION DE NUESTRO DICC ORDENADO ALF.   TYPE: LIST
+def crear_diccionario(archivo_pal,archivo_def):
+    dicc=[]
+    palabra = leer_archivo(archivo_pal)
+    definicion = leer_archivo(archivo_def)
+    while palabra and definicion:
+        #valido que la palabra contenga solo letras y tenga un tamaño mayor al pedido
+        if palabra.isalpha() and len(palabra) >= TAMANIO:
+            dicc.append([palabra, definicion])
+            palabra = leer_archivo(archivo_pal)
+            definicion = leer_archivo(archivo_def)
+        else:   
+            palabra = leer_archivo(archivo_pal)
+            definicion = leer_archivo(archivo_def)
+    dicc.sort(key = lambda x: x[0])
+    return dicc
+
 #------------------------------------------------------------------------------------------------------------------------
 
-#CREO EL ARCHIVO DICCIONARIO.CSV
-def crear_diccionario_csv(palabras_definiciones):
-    diccionario_csv = 'diccionario.csv'
-    #escribir los datos en el archivo CSV
-    with open(diccionario_csv, 'w', newline='',encoding=ENCODING) as dicc_csv:
-        escritor = csv.writer(dicc_csv)
-        escritor.writerows(palabras_definiciones)
-    return diccionario_csv
-
-crear_diccionario_csv(palabras_definiciones)
-
+palabras_txt = open("palabras.txt", "r", encoding="utf8")
+definiciones_txt = open("definiciones.txt", "r", encoding="utf8")
+diccionario_txt = open("diccionario.csv", "w", encoding="utf8")
+palabras_definiciones = crear_diccionario(palabras_txt,definiciones_txt)
+grabar_archivo(diccionario_txt, palabras_definiciones)
+definiciones_txt.close()
+palabras_txt.close()
+diccionario_txt.close()
 
